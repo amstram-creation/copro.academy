07df64dd428870577d8f62fff36431aac4de1930
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-08-25 11:28:00 +0200

    fix: change order of training retrieval to ascending by start date

--- app/io/route/formation/formation.php
+++ app/io/route/formation/formation.php
@@ -6,7 +6,7 @@ return function ($args = null) {
 
     $data = ['title' => 'Formations', 'description' => 'Découvrez nos formations certifiées en gestion de copropriétés. Programmes adaptés aux professionnels de l\'immobilier et conformes à la législation belge.'];
 
-    $data['formation'] = $db->query("SELECT * FROM training_plus WHERE enabled_at IS NOT NULL AND start_date >= CURDATE() ORDER BY start_date DESC LIMIT 10")->fetchAll();
+    $data['formation'] = $db->query("SELECT * FROM training_plus WHERE enabled_at IS NOT NULL AND start_date >= CURDATE() ORDER BY start_date ASC LIMIT 10")->fetchAll();
     $data['formation_niveau'] = (tag_by_parent('formation-niveau'));
     $data['benefits'] = $db->query("SELECT * FROM benefit WHERE is_active = 1 ORDER BY sort_order")->fetchAll();
 
